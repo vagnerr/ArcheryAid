0a1b229b1ecfa5da5a611f688b5f26ffd3127707
Peter Wise <peter@bloodaxe.com>
2017-10-06 17:48:34 +0100

    Adding data contract/helper and initialisation data for Archery rounds

---
+++ mobile/src/main/java/com/vagnerr/android/archeryaid/RoundPickActivity.java
@@ -0,0 +1,13 @@
+package com.vagnerr.android.archeryaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundPickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_pick);
+    }
+}
