f449ec0f5170d40a0b43d3e3bec1a5f0d331748c
Peter Wise <peter@bloodaxe.com>
2017-10-06 17:51:20 +0100

    Adding data contract/helper and initialisation data for Archery rounds

--- mobile/src/main/java/com/vagnerr/android/archeryaid/data/ArcheryContract.java
+++ mobile/src/main/java/com/vagnerr/android/archeryaid/data/ArcheryContract.java
@@ -11,12 +11,30 @@ import android.text.format.Time;
  */
 
 public class ArcheryContract {
+    // Generic
     public static final String CONTENT_AUTHORITY = "com.vagnerr.android.archeryaid";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Arrow Counting: ArrowCount
     public static final String PATH_ARROWCOUNT = "arrowcount";
     public static final String PATH_ARROWCOUNTHISTORY = "arrowcount/history";
-    //public static final String PATH_LOCATION = "location";
 
+    // Round Definition:
+    private static final String PATH_ROUNDMAKEUP        = "roundmakeup";
+    private static final String PATH_TARGETTYPECONST    = "targettypeconst";
+    private static final String PATH_ROUNDCONST         = "roundconst";
+    private static final String PATH_RULESCONST         = "rulesconst";
+
+    // Shooting Session:
+    private static final String PATH_END            = "end";
+    private static final String PATH_SESSION        = "session";
+    private static final String PATH_SESSIONMAKEUP  = "sessionmakeup";
+
+
+    // Other Constants:
+    private static final String PATH_ARROWCONST             = "arrowconst";
+    private static final String PATH_CLASSIFICATIONCONST    = "classificationconst";
+    private static final String PATH_SESSIONSTATECONST      = "sessionstateconst";
 
     // To make it easy to query for the exact date items, we normalise dates on certain data
     // ( eg arrow counts per day ) that go into the database to the start of the the Julian day at UTC.
@@ -30,7 +48,17 @@ public class ArcheryContract {
 
 
 
+/*
 
+   #                                    #####
+  # #   #####  #####   ####  #    #    #     #  ####  #    # #    # ##### # #    #  ####
+ #   #  #    # #    # #    # #    #    #       #    # #    # ##   #   #   # ##   # #    #
+#     # #    # #    # #    # #    #    #       #    # #    # # #  #   #   # # #  # #
+####### #####  #####  #    # # ## #    #       #    # #    # #  # #   #   # #  # # #  ###
+#     # #   #  #   #  #    # ##  ##    #     # #    # #    # #   ##   #   # #   ## #    #
+#     # #    # #    #  ####  #    #     #####   ####   ####  #    #   #   # #    #  ####
+
+*/
     /*  Arrow counts table recording simple number of arrows per day, Current count
         will be the record for "today's date"
      */
@@ -68,6 +96,220 @@ public class ArcheryContract {
                         .build();
 
         }
+    }
+
+/*
+######                                 ######
+#     #  ####  #    # #    # #####     #     # ###### ###### # #    # # ##### #  ####  #    #
+#     # #    # #    # ##   # #    #    #     # #      #      # ##   # #   #   # #    # ##   #
+######  #    # #    # # #  # #    #    #     # #####  #####  # # #  # #   #   # #    # # #  #
+#   #   #    # #    # #  # # #    #    #     # #      #      # #  # # #   #   # #    # #  # #
+#    #  #    # #    # #   ## #    #    #     # #      #      # #   ## #   #   # #    # #   ##
+#     #  ####   ####  #    # #####     ######  ###### #      # #    # #   #   #  ####  #    #
+ */
+
+    public static final class RoundMakeup implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_ROUNDMAKEUP).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ROUNDMAKEUP;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ROUNDMAKEUP;
+
+        public static final String TABLE_NAME               = "round_makeup";
+        public static final String COLUMN_ROUND_ID          = "round_id";               // FK: round_const
+        public static final String COLUMN_TARGET_TYPE_ID    = "target_type_id";         // FK: target_type_const
+        public static final String COLUMN_END_COUNT         = "end_count";
+        public static final String COLUMN_DISTANCE_ORDER    = "distance_order";
+
+
+    }
+
+    public static final class TargetTypeConst implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TARGETTYPECONST).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TARGETTYPECONST;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TARGETTYPECONST;
+
+        public static final String TABLE_NAME               = "target_type_const";
+        public static final String COLUMN_DISTANCE          = "distance";
+        public static final String COLUMN_UNITS             = "units";       // Or units_id as a FK?
+        public static final String COLUMN_TARGET_SIZE       = "target_size";
+        public static final String COLUMN_ZONE_COUNT        = "zone_count";
+
+
+    }
+
+
+    public static final class RoundConst implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_ROUNDCONST).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ROUNDCONST;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ROUNDCONST;
+
+        public static final String TABLE_NAME       = "round_const";
+        public static final String COLUMN_NAME      = "name";
+        public static final String COLUMN_OFFICIAL  = "official";
+        public static final String COLUMN_CUSTOM    = "custom";
+        public static final String COLUMN_RULES_ID  = "rules_id";       // FK: rules_const
+
+
+
+    }
+
+    public static final class RulesConst implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_RULESCONST).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RULESCONST;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RULESCONST;
+
+        public static final String TABLE_NAME       = "rules_const";
+        public static final String COLUMN_NAME      = "name";
+
+
+    }
+
+
+/*
+ #####                                                 #####
+#     # #    #  ####   ####  ##### # #    #  ####     #     # ######  ####   ####  #  ####  #    #
+#       #    # #    # #    #   #   # ##   # #    #    #       #      #      #      # #    # ##   #
+ #####  ###### #    # #    #   #   # # #  # #          #####  #####   ####   ####  # #    # # #  #
+      # #    # #    # #    #   #   # #  # # #  ###          # #           #      # # #    # #  # #
+#     # #    # #    # #    #   #   # #   ## #    #    #     # #      #    # #    # # #    # #   ##
+ #####  #    #  ####   ####    #   # #    #  ####      #####  ######  ####   ####  #  ####  #    #
+ */
+
+    public static final class End implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_END).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_END;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_END;
+
+        public static final String TABLE_NAME           = "end";
+        public static final String COLUMN_SESSION_ID    = "session_id";     // FK: session
+        public static final String COLUMN_END           = "end";
+        public static final String COLUMN_ARROW1        = "arrow1";
+        public static final String COLUMN_ARROW2        = "arrow2";
+        public static final String COLUMN_ARROW3        = "arrow3";
+        public static final String COLUMN_ARROW4        = "arrow4";
+        public static final String COLUMN_ARROW5        = "arrow5";
+        public static final String COLUMN_ARROW6        = "arrow6";
+
+
+    }
+
+    public static final class Session implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_SESSION).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SESSION;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SESSION;
+
+        public static final String TABLE_NAME               = "session";
+        public static final String COLUMN_DATE              = "date";
+        public static final String COLUMN_DATE_FINISHED     = "date_finished";
+        public static final String COLUMN_WEATHER           = "weather";
+        public static final String COLUMN_ROUND_ID          = "round_id";           // FK: round_const
+        public static final String COLUMN_SESSION_STATE_ID  = "session_state_id";   // FK: session_state_const
+        public static final String COLUMN_TOTAL_SCORE       = "total_score";
+        public static final String COLUMN_TOTAL_X           = "total_x";
+        public static final String COLUMN_TOTAL_10          = "total_10";
+        public static final String COLUMN_TOTAL_GOLD        = "total_gold";
+        public static final String COLUMN_TOTAL_ARROWS      = "total_arrows";
+        public static final String COLUMN_TOTAL_HITS        = "total_hits";
+        public static final String COLUMN_TOTAL_SIGHTERS    = "total_sighters";
+        public static final String COLUMN_ROUND_COMPLETE    = "round_complete";
+        public static final String COLUMN_CLASSIFICATION_ID = "classification_id";  // FK: classification_const
+        public static final String COLUMN_HANDICAP          = "handicap";
+
+
+    }
+
+    public static final class SessionMakeup implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_SESSIONMAKEUP).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SESSIONMAKEUP;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SESSIONMAKEUP;
+
+        public static final String TABLE_NAME               = "session_makeup";
+        public static final String COLUMN_SESSION_ID        = "session_id";     // FK: session
+        public static final String COLUMN_TARGET_TYPE_ID    = "target_type_id"; // FK: target_type_const
+        public static final String COLUMN_END_COUNT         = "end_count";
+        public static final String COLUMN_DISTANCE_ORDER    = "distance_order";
+
+
+    }
+
+
+
+/*
+#######                                #####
+#     # ##### #    # ###### #####     #     #  ####  #    #  ####  #####   ##   #    # #####  ####
+#     #   #   #    # #      #    #    #       #    # ##   # #        #    #  #  ##   #   #   #
+#     #   #   ###### #####  #    #    #       #    # # #  #  ####    #   #    # # #  #   #    ####
+#     #   #   #    # #      #####     #       #    # #  # #      #   #   ###### #  # #   #        #
+#     #   #   #    # #      #   #     #     # #    # #   ## #    #   #   #    # #   ##   #   #    #
+#######   #   #    # ###### #    #     #####   ####  #    #  ####    #   #    # #    #   #    ####
+ */
+
+    public static final class ArrowConst implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_ARROWCONST).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ARROWCONST;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ARROWCONST;
+
+        public static final String TABLE_NAME       = "arrow_const";
+        public static final String COLUMN_NAME      = "name";
+        public static final String COLUMN_VALUE     = "value";
+    }
+
+    public static final class ClassificationConst implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_CLASSIFICATIONCONST).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_CLASSIFICATIONCONST;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_CLASSIFICATIONCONST;
+
+        public static final String TABLE_NAME       = "classification_const";
+        public static final String COLUMN_NAME      = "name";
+    }
+
+    public static final class SessionStateConst implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_SESSIONSTATECONST).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SESSIONSTATECONST;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SESSIONSTATECONST;
+
+        public static final String TABLE_NAME       = "session_state_const";
+        public static final String COLUMN_NAME      = "name";
+        public static final String COLUMN_OFFICIAL  = "official";
     }
 
 
--- mobile/src/main/java/com/vagnerr/android/archeryaid/data/ArcheryDbHelper.java
+++ mobile/src/main/java/com/vagnerr/android/archeryaid/data/ArcheryDbHelper.java
@@ -3,15 +3,22 @@ package com.vagnerr.android.archeryaid.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 
 import com.vagnerr.android.archeryaid.data.ArcheryContract.*;
 
 /**
  * Created by Peter on 11/09/2017.
+ * Database creation / update routines
  */
 
 public class ArcheryDbHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 2;
+
+    private static final int DATABASE_VERSION = 3;
+
+    private final String LOG_TAG = ArcheryDbHelper.class.getSimpleName();
+
     static final String DATABASE_NAME = "archeryaid.db";
 
     public ArcheryDbHelper(Context context) {
@@ -20,6 +27,247 @@ public class ArcheryDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "onCreate: start");
+        createArrowCount(sqLiteDatabase);
+        createRoundDefinition(sqLiteDatabase);
+        createOtherConst(sqLiteDatabase);
+        createShootingSession(sqLiteDatabase);
+        Log.v(LOG_TAG, "onCreate: end");
+
+    }
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        Log.v(LOG_TAG, "onUpgrade: start");
+
+        // Version 2: was first version, just the arrow counter
+        //            theoretically redundant as there was no version 1, but here for completeness
+        if (oldVersion < 2) {
+            Log.v(LOG_TAG, "   : <2");
+
+            createArrowCount(sqLiteDatabase);
+        }
+        // Version 3: Added first cut of Round Definition and Shooting Session tables
+        if (oldVersion < 3) {
+            Log.v(LOG_TAG, "   : <3");
+            createRoundDefinition(sqLiteDatabase);
+            createOtherConst(sqLiteDatabase);
+            createShootingSession(sqLiteDatabase);
+        }
+        Log.v(LOG_TAG, "onUpgrade: end");
+
+    }
+
+
+    private void createRoundDefinition(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createRoundDefinition: start");
+
+        createTargetTypeConst(sqLiteDatabase);
+        createRulesConst(sqLiteDatabase);
+        createRoundConst(sqLiteDatabase);
+        createRoundMakeup(sqLiteDatabase);
+        Log.v(LOG_TAG, "createRoundDefinition: end");
+    }
+
+    private void createOtherConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createOtherConst: start");
+        createArrowConst(sqLiteDatabase);
+        createClassificationConst(sqLiteDatabase);
+        createSessionStateConst(sqLiteDatabase);
+    }
+
+    private void createShootingSession(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createShootingSession: start");
+        createSession(sqLiteDatabase);
+        createEnd(sqLiteDatabase);
+        createSessionMakeup(sqLiteDatabase);
+    }
+
+
+    private void createArrowConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createArrowConst: start");
+        final String SQL_CREATE = "CREATE TABLE " + ArrowConst.TABLE_NAME + " (" +
+                ArrowConst._ID + " INTEGER PRIMARY KEY, " +
+                ArrowConst.COLUMN_NAME + " TEXT NOT NULL, " +
+                ArrowConst.COLUMN_VALUE + " INTEGER NOT NULL" +
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+    private void createClassificationConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createClassificationConst: start");
+        final String SQL_CREATE = "CREATE TABLE " + ClassificationConst.TABLE_NAME + " (" +
+                ClassificationConst._ID + " INTEGER PRIMARY KEY, " +
+                ClassificationConst.COLUMN_NAME + " TEXT NOT NULL " +
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+    private void createSessionStateConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createSessionStateConst: start");
+        final String SQL_CREATE = "CREATE TABLE " + SessionStateConst.TABLE_NAME + " (" +
+                SessionStateConst._ID + " INTEGER PRIMARY KEY, " +
+                SessionStateConst.COLUMN_NAME + " TEXT NOT NULL, " +
+                SessionStateConst.COLUMN_OFFICIAL + " INTEGER NOT NULL "+
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+
+    private void createSession(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createSession: start");
+        final String SQL_CREATE = "CREATE TABLE " + Session.TABLE_NAME + " (" +
+                Session._ID + " INTEGER PRIMARY KEY, " +
+                Session.COLUMN_DATE + " INTEGER NOT NULL, " +
+                Session.COLUMN_DATE_FINISHED + " INTEGER, " +   // Null if session was recorded simply as a start with now duration timings
+                Session.COLUMN_WEATHER + " TEXT, " +
+                Session.COLUMN_ROUND_ID + " INTEGER NOT NULL, " +
+                Session.COLUMN_SESSION_STATE_ID + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_SCORE + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_X + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_10 + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_GOLD + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_ARROWS + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_HITS + " INTEGER NOT NULL, " +
+                Session.COLUMN_TOTAL_SIGHTERS + " INTEGER NOT NULL, " +
+                Session.COLUMN_ROUND_COMPLETE + " INTEGER NOT NULL, " +
+                Session.COLUMN_CLASSIFICATION_ID + " INTEGER, " +     // For when I get clasification tables
+                Session.COLUMN_HANDICAP + " INTEGER, " +              // For when I get handicap tables
+
+                " FOREIGN KEY (" + Session.COLUMN_ROUND_ID + ") REFERENCES " +
+                RoundConst.TABLE_NAME + " (" + RoundConst._ID + "), " +
+                " FOREIGN KEY (" + Session.COLUMN_SESSION_STATE_ID + ") REFERENCES " +
+                SessionStateConst.TABLE_NAME + " (" + SessionStateConst._ID + "), " +
+                " FOREIGN KEY (" + Session.COLUMN_CLASSIFICATION_ID + ") REFERENCES " +
+                ClassificationConst.TABLE_NAME + " (" + ClassificationConst._ID + ") " +
+
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+
+    }
+
+    private void createEnd(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createEnd: start");
+        final String SQL_CREATE = "CREATE TABLE " + End.TABLE_NAME + " (" +
+                End._ID + " INTEGER PRIMARY KEY, " +
+                End.COLUMN_SESSION_ID + " INTEGER NOT NULL, " +
+                End.COLUMN_END + " INTEGER NOT NULL, " +
+                End.COLUMN_ARROW1 + " INTEGER, " +
+                End.COLUMN_ARROW2 + " INTEGER, " +
+                End.COLUMN_ARROW3 + " INTEGER, " +
+                End.COLUMN_ARROW4 + " INTEGER, " +
+                End.COLUMN_ARROW5 + " INTEGER, " +
+                End.COLUMN_ARROW6 + " INTEGER, " +
+
+                " FOREIGN KEY (" + End.COLUMN_SESSION_ID + ") REFERENCES " +
+                Session.TABLE_NAME + " (" + Session._ID + "), " +
+
+                " FOREIGN KEY (" + End.COLUMN_ARROW1 + ") REFERENCES " +
+                ArrowConst.TABLE_NAME + " (" + ArrowConst._ID + "), " +
+                " FOREIGN KEY (" + End.COLUMN_ARROW2 + ") REFERENCES " +
+                ArrowConst.TABLE_NAME + " (" + ArrowConst._ID + "), " +
+                " FOREIGN KEY (" + End.COLUMN_ARROW3 + ") REFERENCES " +
+                ArrowConst.TABLE_NAME + " (" + ArrowConst._ID + "), " +
+                " FOREIGN KEY (" + End.COLUMN_ARROW4 + ") REFERENCES " +
+                ArrowConst.TABLE_NAME + " (" + ArrowConst._ID + "), " +
+                " FOREIGN KEY (" + End.COLUMN_ARROW5 + ") REFERENCES " +
+                ArrowConst.TABLE_NAME + " (" + ArrowConst._ID + "), " +
+                " FOREIGN KEY (" + End.COLUMN_ARROW6 + ") REFERENCES " +
+                ArrowConst.TABLE_NAME + " (" + ArrowConst._ID + ") " +
+
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+
+    }
+
+    private void createSessionMakeup(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createSessionMakeup: start");
+        final String SQL_CREATE = "CREATE TABLE " + SessionMakeup.TABLE_NAME + " (" +
+                SessionMakeup._ID + " INTEGER PRIMARY KEY, " +
+                SessionMakeup.COLUMN_SESSION_ID + " INTEGER NOT NULL, " +
+                SessionMakeup.COLUMN_TARGET_TYPE_ID + " INTEGER NOT NULL, " +
+                SessionMakeup.COLUMN_END_COUNT + " INTEGER NOT NULL, " +
+                SessionMakeup.COLUMN_DISTANCE_ORDER + " INTEGER NOT NULL, " +
+
+                " FOREIGN KEY (" + SessionMakeup.COLUMN_SESSION_ID + ") REFERENCES " +
+                Session.TABLE_NAME + " (" + Session._ID + ")," +
+                " FOREIGN KEY (" + SessionMakeup.COLUMN_TARGET_TYPE_ID + ") REFERENCES " +
+                TargetTypeConst.TABLE_NAME + " (" + TargetTypeConst._ID + ")" +
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+
+    private void createTargetTypeConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createTargetTypeConst: start");
+        final String SQL_CREATE = "CREATE TABLE " + TargetTypeConst.TABLE_NAME + " (" +
+                TargetTypeConst._ID + " INTEGER PRIMARY KEY, " +
+                TargetTypeConst.COLUMN_DISTANCE + " INTEGER NOT NULL, " +
+                TargetTypeConst.COLUMN_UNITS + " TEXT NOT NULL, " +             // TODO: Do I want this to be an ID?
+                TargetTypeConst.COLUMN_TARGET_SIZE + " INTEGER NOT NULL, " +    // In CM TODO: an ID?
+                TargetTypeConst.COLUMN_ZONE_COUNT + " INTEGER NOT NULL " +      // TODO: May want to change this to an ID ( const table specific for listing zones ) or a string listing available zones
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+    private void createRulesConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createRulesConst: start");
+        final String SQL_CREATE = "CREATE TABLE " + RulesConst.TABLE_NAME + " (" +
+                RulesConst._ID + " INTEGER PRIMARY KEY, " +
+                RulesConst.COLUMN_NAME + " TEXT NOT NULL " +
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+    }
+
+
+    private void createRoundConst(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createRoundConst: start");
+        final String SQL_CREATE = "CREATE TABLE " + RoundConst.TABLE_NAME + " (" +
+                RoundConst._ID + " INTEGER PRIMARY KEY, " +
+                RoundConst.COLUMN_NAME + " TEXT NOT NULL, " +
+                RoundConst.COLUMN_OFFICIAL + " INTEGER NOT NULL, " + // Boolean
+                RoundConst.COLUMN_CUSTOM + " INTEGER NOT NULL, " + // Boolean
+                RoundConst.COLUMN_RULES_ID + " INTEGER NOT NULL, " +
+
+                " FOREIGN KEY (" + RoundConst.COLUMN_RULES_ID + ") REFERENCES " +
+                    RulesConst.TABLE_NAME + " (" + RulesConst._ID + ")" +
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+
+    }
+
+    private void createRoundMakeup(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createRoundMakeup: start");
+        final String SQL_CREATE = "CREATE TABLE " + RoundMakeup.TABLE_NAME + " (" +
+                RoundMakeup._ID + " INTEGER PRIMARY KEY, " +
+                RoundMakeup.COLUMN_ROUND_ID + " INTEGER NOT NULL, " +
+                RoundMakeup.COLUMN_TARGET_TYPE_ID + " INTEGER NOT NULL, " +
+                RoundMakeup.COLUMN_END_COUNT + " INTEGER NOT NULL, " +
+                RoundMakeup.COLUMN_DISTANCE_ORDER + " INTEGER NOT NULL, " +
+
+                " FOREIGN KEY (" + RoundMakeup.COLUMN_ROUND_ID + ") REFERENCES " +
+                    RoundConst.TABLE_NAME + " (" + RoundConst._ID + ") ," +
+                " FOREIGN KEY (" + RoundMakeup.COLUMN_TARGET_TYPE_ID + ") REFERENCES " +
+                    TargetTypeConst.TABLE_NAME + " (" + TargetTypeConst._ID + ")" +
+
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE);
+
+    }
+
+    private void createArrowCount(SQLiteDatabase sqLiteDatabase) {
+        Log.v(LOG_TAG, "createArrowCount: start");
         final String SQL_CREATE_ARROWCOUNT_TABLE = "CREATE TABLE " + ArrowCount.TABLE_NAME + " (" +
                 ArrowCount._ID + " INTEGER PRIMARY KEY, " +
                 ArrowCount.COLUMN_DATE + " INTEGER UNIQUE NOT NULL, "+
@@ -27,15 +275,8 @@ public class ArcheryDbHelper extends SQLiteOpenHelper {
                 " );";
 
         sqLiteDatabase.execSQL(SQL_CREATE_ARROWCOUNT_TABLE);
-
     }
 
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        // In development we don't need to retain data
-        // TODO: rewrite this on (2nd) release.
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ArrowCount.TABLE_NAME);
-        onCreate(sqLiteDatabase);
-    }
+
 }
