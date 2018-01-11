291f6ed08269bf0dc1e2baf1081859ace45007b5
Peter Wise <peter@bloodaxe.com>
2018-01-11 20:11:42 +0000

    Working POC, programatically adding sub view elements
    Extra "ends" added through code hardcoded for now, but do work and all is visible. Its not yet dynamically building based on requested round though

--- mobile/src/main/java/com/vagnerr/android/archeryaid/RoundActivity.java
+++ mobile/src/main/java/com/vagnerr/android/archeryaid/RoundActivity.java
@@ -3,9 +3,15 @@ package com.vagnerr.android.archeryaid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class RoundActivity extends AppCompatActivity {
     private final String LOG_TAG = RoundActivity.class.getSimpleName();
@@ -34,5 +40,42 @@ public class RoundActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        // Build out the ends
+        LinearLayout ends = (LinearLayout) findViewById(R.id.roundDistances);
+        //POC...
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        Button button = new Button(this);
+        button.setText("Button 1");
+        int id = 1;
+        button.setId(id);
+        //ends.linearLayout
+
+        // False: don't add view straight to layout. if you do that then I think it returns
+        //        a singleton so it cant "add" second one later
+        View endview = inflater.inflate(R.layout.content_round_end, ends, false);
+        endview.setId(2);
+        View endview2 = inflater.inflate(R.layout.content_round_end, ends, false);
+        endview2.setId(3);
+
+        Button button2 = new Button(this);
+        button2.setText("Button 2");
+        int id2 = 3;
+        button2.setId(id2);
+        //ends.linearLayout
+        ends.addView(button2, -1, WRAP_CONTENT);
+
+        ends.addView( endview2, -1, WRAP_CONTENT );  // -1: add to end else the overwrite each other?
+
+        ends.addView(button, -1, WRAP_CONTENT);
+
+        ends.addView( endview, -1, WRAP_CONTENT );
+Log.v(LOG_TAG,"Done");
+
+
+
     }
+
+
 }
