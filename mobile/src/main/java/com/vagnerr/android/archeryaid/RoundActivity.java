1accbed368eac7d3ff3991b1751289409d9e9181
Peter Wise <peter@bloodaxe.com>
2018-01-12 17:09:49 +0000

    Working POC, programatically builds full round from static config values

--- mobile/src/main/java/com/vagnerr/android/archeryaid/RoundActivity.java
+++ mobile/src/main/java/com/vagnerr/android/archeryaid/RoundActivity.java
@@ -31,17 +31,15 @@ public class RoundActivity extends AppCompatActivity {
         ruleID = getIntent().getLongExtra(RULE_ID, -1);
         roundID = getIntent().getLongExtra(ROUND_ID, -1);
 
-        Log.v(LOG_TAG, "Creating new round: rule:" + ruleID + " round:" +roundID);
+        Log.v(LOG_TAG, "Creating new round: rule:" + ruleID + " round:" + roundID);
         //TODO: if rule or round are -1 we need to abort. *should* never happen but you never know
 
         setContentView(R.layout.activity_round);
 
-        mAdView = (AdView)findViewById(R.id.adViewRound);  // must come after setContentView or you get null
+        mAdView = (AdView) findViewById(R.id.adViewRound);  // must come after setContentView or you get null
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        // Build out the ends
-        LinearLayout ends = (LinearLayout) findViewById(R.id.roundDistances);
         //POC...
 
         // TODO:
@@ -60,33 +58,61 @@ public class RoundActivity extends AppCompatActivity {
         //         * On initial creation of the round generate "zeros" in the DB for the new round then...
         //           * attach the cursors as we build out the GUI?
         //       Going to need some testing on this especially from a performance perspective.
+        // Build out the ends
 
+        // TEMPORARY TEST DATA
+        Integer[][] TEST_DATA =new Integer[][]{ // Full Metric Round
+                {90, 6},
+                {70, 6},
+                {50, 6},
+                {30, 6},
+        };
+
+
+        LinearLayout distances = (LinearLayout) findViewById(R.id.roundDistances);
         LayoutInflater inflater = getLayoutInflater();
-        Button button = new Button(this);
-        button.setText("Button 1");
-        int id = 1;
-        button.setId(id);
-        //ends.linearLayout
+        int my_ids=1;
 
-        // False: don't add view straight to layout. if you do that then I think it returns
-        //        a singleton so it cant "add" second one later
-        View endview = inflater.inflate(R.layout.content_round_end, ends, false);
-        endview.setId(2);
-        View endview2 = inflater.inflate(R.layout.content_round_end, ends, false);
-        endview2.setId(3);
+        for ( int distance = 0; distance < TEST_DATA.length; distance++){
+            View distanceview = inflater.inflate(R.layout.content_round_distance, distances, false);
+            distanceview.setId(my_ids++);
+            distances.addView(distanceview, -1, WRAP_CONTENT);
 
-        Button button2 = new Button(this);
-        button2.setText("Button 2");
-        int id2 = 3;
-        button2.setId(id2);
-        //ends.linearLayout
-        ends.addView(button2, -1, WRAP_CONTENT);
+            LinearLayout ends = distanceview.findViewById(R.id.distance_ends);
+            for ( int end = 0; end < TEST_DATA[distance][1] ; end++){
+                View endview = inflater.inflate(R.layout.content_round_end, distances, false);
+                endview.setId(my_ids++);
+                ends.addView(endview,-1,WRAP_CONTENT);
+            }
+        }
 
-        ends.addView( endview2, -1, WRAP_CONTENT );  // -1: add to end else the overwrite each other?
+//        Button button = new Button(this);
+//        button.setText("Button 1");
+//        int id = 1;
+//        button.setId(id);
+//        //ends.linearLayout
+//
+//        // False: don't add view straight to layout. if you do that then I think it returns
+//        //        a singleton so it cant "add" second one later
+//        View endview = inflater.inflate(R.layout.content_round_end, distances, false);
+//        endview.setId(2);
+//        View endview2 = inflater.inflate(R.layout.content_round_end, distances, false);
+//        endview2.setId(3);
+//
+//        Button button2 = new Button(this);
+//        button2.setText("Button 2");
+//        int id2 = 3;
+//        button2.setId(id2);
+//        //ends.linearLayout
+//        distances.addView(button2, -1, WRAP_CONTENT);
+//
+//        distances.addView( endview2, -1, WRAP_CONTENT );  // -1: add to end else the overwrite each other?
+//
+//        distances.addView(button, -1, WRAP_CONTENT);
+//
+//        distances.addView( endview, -1, WRAP_CONTENT );
 
-        ends.addView(button, -1, WRAP_CONTENT);
 
-        ends.addView( endview, -1, WRAP_CONTENT );
 Log.v(LOG_TAG,"Done");
 
 
